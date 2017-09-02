f35a9901d130a047190ccb0607a31868d2d5b43c
Jeandre Rohlandt <jrohlandt@gmail.com>
2017-09-02 13:44:46 +0200

    database config file

--- app.js
+++ app.js
@@ -58,12 +58,18 @@ app.use(express.static(path.join(__dirname, '/public')))
 | SESSIONS
 |-------------------------------------------------------------------------------
 */
+
+//
+var dbConfig = require('./config/database').config
+//
+console.log(dbConfig.port)
+
 var sessionStoreOptions = {
-  host: 'localhost',
-  port: '3306',
-  user: 'homestead',
-  password: 'secret',
-  database: 'node_mysql',
+  host: dbConfig.host,
+  port: dbConfig.port,
+  user: dbConfig.username,
+  password: dbConfig.password,
+  database: dbConfig.database,
   checkExpirationInterval: 900000, // How frequently expired sessions will be cleared; milliseconds.
   expiration: 86400000, // The maximum age of a valid session; milliseconds.
   createDatabaseTable: true, // Whether or not to create the sessions database table, if one does not already exist.
