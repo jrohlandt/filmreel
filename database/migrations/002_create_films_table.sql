c3a658901547648a0fc11209529f1ce3b482f56e
Jeandre Rohlandt <jrohlandt@gmail.com>
2017-09-12 22:17:17 +0200

    add poster_image colum to films table

--- database/migrations/002_create_films_table.sql
+++ database/migrations/002_create_films_table.sql
@@ -2,6 +2,7 @@ CREATE TABLE `films` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(100) NOT NULL,
   `year` year(4) DEFAULT '0000',
+  `poster_image` varchar(256) DEFAULT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
