a6f5d8e58269e565f82e467cf5bb1a4df8993835
Jeandre Rohlandt <jrohlandt@gmail.com>
2017-09-26 20:16:01 +0200

    fix sql query

---
+++ database/migrations/20170911_1_create_users_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(256) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
