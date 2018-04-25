d2b7c284578a5ce2b63b7c5c2046a346662fa3c7
Jeandre Rohlandt <jrohlandt@gmail.com>
2018-04-25 10:55:39 +0200

    starting basic components

--- resources/assets/admin/js/app.js
+++ resources/assets/admin/js/app.js
@@ -1,1 +1,34 @@
-console.log('app');
+import { BrowserRouter } from 'react-router-dom';
+
+
+const App = () => (
+    <div>
+        <Header />
+        <SideBar />
+        <Main />
+    </div>
+);
+
+const Header = () => (<div><h1>Header</h1></div>);
+
+const SideBar = () => (
+    <div>
+        <ul>
+            <li>item 1</li>
+        </ul>
+    </div>
+);
+
+const Main = () => (
+    <Switch>
+        <Route exact path='/admin/' component={Dashboard} />
+        <Route exact path='/admin/films' component={FilmsList} />
+        <Route path='/admin/films/:id' component={FilmShow} />
+    </Switch>
+);
+
+ReactDOM.render((
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+), document.getElementById('app'));
